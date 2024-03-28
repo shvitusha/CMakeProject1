19331e961600062d9e7e25622768d5be744a5152
sh.vitusha <sh.vitusha@mail.ru>
2024-03-28 16:27:52 +0300

    end

--- args_parse/ArgsParser.cpp
+++ args_parse/ArgsParser.cpp
@@ -40,7 +40,7 @@ namespace args_parse {
 		std::cout << "Supported commands:" << std::endl;
 		for (const auto& arg : _args)
 		{
-			if (arg.GetShortName() == '\0')
+			if (!arg.GetShortName() == '\0')
 				std::cout << " -" << arg.GetShortName() << " ";
 			std::cout << " --" << arg.GetLongName() << "\t" << arg.GetDescription() << std::endl;
 		}
@@ -127,16 +127,40 @@ namespace args_parse {
 			std::string argValue;
 
 			if (o_type == "long") {
-				argName = argStr.substr(2);
+				size_t equalPosition = argStr.find('=');
+				size_t spacePosition = argStr.find(' ');
+
+				if (equalPosition != std::string::npos) {
+					argName = argStr.substr(2, equalPosition - 2);
+					argValue = argStr.substr(equalPosition + 2);
+					std::cout << argValue << std::endl;
+				}
+				else
+				{
+					if (spacePosition != std::string::npos) {
+						argName = argStr.substr(2, spacePosition - 2);
+						argValue = argStr.substr(spacePosition + 2);
+						std::cout << argValue << std::endl;
+					}
+					else
+					{
+						argName = argStr.substr(2);
+						if (argName.length() > 0) {
+							argValue = argStr.substr(2 + argName.length());
+						}
+					}
+				}
 			}
 			else if (o_type == "short") {
-				argName = argStr.substr(2);
-				if (argStr.length() > 2 && argStr.substr(2, 1) == "=") {
+				argName = argStr.substr(1, 1);
+				if (argStr.length() > 2 && (argStr.substr(2, 1) == "=" || argStr.substr(2, 1) == " ")) {
 					argValue = argStr.substr(3);
+					std::cout << argValue << std::endl;
 				}
-				else if(argStr.length() > 2)
+				else if (argStr.length() > 2)
 				{
 					argValue = argStr.substr(2);
+					std::cout << argValue << std::endl;
 				}
 			}
 			else
